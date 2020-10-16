36e5854c2c8dfe465bfa93e89979b7a00c8198ff
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-16 14:52:00 -0500

    feat: created contact.txt and wrote list to file

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.nio.file.Files.write;
+
 public class ContactApp {
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         // Directory for contact output
 
         // Methods for
@@ -12,5 +23,30 @@ public class ContactApp {
 
         Amanda.printContact();
         Levi.printContact();
+
+
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if (! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
+        //printing to contacts.txt
+        List<String> contactList = Arrays.asList("name1", "name2", "bacon");
+        Files.write(dataFile, contactList);
+
+        //printing to console
+        List<String> fileContents = Files.readAllLines(dataFile);
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
     }
 }
