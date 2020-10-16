bfab56c2e5de71d5dabfb4ca67cf5b907b679079
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-16 14:13:13 -0500

    feat: added Contact, ContactList, and Main app class files, tested contact class

---
+++ src/Contact.java
@@ -0,0 +1,29 @@
+public class Contact {
+    private String name, phoneNumber;
+
+    public Contact(String cName, String cPhone){
+        this.name = cName;
+        this.phoneNumber = cPhone;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public void setName(String cName){
+        this.name = cName;
+    }
+
+    public void setPhoneNumber(String cPhone){
+        this.phoneNumber = cPhone;
+    }
+
+    public void printContact(){
+        System.out.printf("%s  | \uD83D\uDCF1 %s |\n", this.name, this.phoneNumber);
+    }
+
+}
---
+++ src/ContactApp.java
@@ -0,0 +1,16 @@
+public class ContactApp {
+    public static void main(String[] args) {
+        // Directory for contact output
+
+        // Methods for
+        // Printing/Viewing, adding, searching, and updating/deleting a contact.
+        // Hashmap <Name, contactObj or PhoneNumber>
+
+        Contact Amanda = new Contact("Amanda", "5555555");
+        Contact Levi = new Contact("Levi", "2222222");
+
+
+        Amanda.printContact();
+        Levi.printContact();
+    }
+}
