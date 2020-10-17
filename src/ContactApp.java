389b79054fe085354302d23fe9d33e78f78a0567
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-17 11:44:25 -0500

    feat: added ContactList class and associated methods for working with the list and its members

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -6,11 +6,27 @@ public class ContactApp {
         // Printing/Viewing, adding, searching, and updating/deleting a contact.
         // Hashmap <Name, contactObj or PhoneNumber>
 
+        // Create contact objects for testing
         Contact Amanda = new Contact("Amanda", "5555555");
         Contact Levi = new Contact("Levi", "2222222");
+        Contact David = new Contact("David", "8675309");
 
+        // Create our list HashMap with our contact objects
+        ContactList list = new ContactList(Amanda, Levi);
 
-        Amanda.printContact();
-        Levi.printContact();
+        // See our output using 2 contacts
+        list.printContactMap();
+
+        // Add a 3rd contact
+        list.addContact(David);
+
+        // Print again
+        list.printContactMap();
+
+        // Remove David
+        list.delContact(David);
+
+        // Print one last time
+        list.printContactMap();
     }
 }
--- src/ContactList.java
+++ src/ContactList.java
@@ -1,2 +1,37 @@
+import java.util.HashMap;
+
 public class ContactList {
+
+    // After further testing, wasn't sure we needed an actual arrayList
+    // Decided to simplify and just use a HashMap with names as the key, and contact objects as values
+    private HashMap<String, Contact> contactList = new HashMap<>();
+
+    public ContactList(Contact...contacts){
+        for (Contact contact: contacts){
+            addContact(contact);
+        }
+    }
+
+    public void addContact(Contact newContact){
+        contactList.put(newContact.getName(), newContact);
+    }
+
+    public void delContact(Contact rmContact){
+        if (contactList.containsKey(rmContact.getName())){
+            contactList.remove(rmContact.getName());
+        } else {
+            System.err.printf("%s is not a valid contact", rmContact.getName());
+        }
+    }
+
+    public void printContactMap(){
+        // %-15s = 15 character columns, - is left justified
+        System.out.printf("%-15s | %-15s%n", "Name", "Phone" );
+        System.out.println("---------------------------------");
+        for (String key : contactList.keySet()){
+            System.out.printf("%-15s | %-15s%n", key, contactList.get(key).getPhoneNumber());
+        }
+        System.out.println("---------------------------------");
+        System.out.println();
+    }
 }
