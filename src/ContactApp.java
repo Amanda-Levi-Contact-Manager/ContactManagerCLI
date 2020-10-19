3b2e74abe392aaecaec93499648905d3dedded2e
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-19 09:38:51 -0500

    feat: Input class, FileIO class, main app adjustments to refactor and clean up

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -1,57 +1,31 @@
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
-
-import static java.nio.file.Files.write;
 
 public class ContactApp {
 
 
     public static void main(String[] args) throws IOException {
-        // Directory for contact output
+
         Input input = new Input();
-        // Methods for
-        // Printing/Viewing, adding, searching, and updating/deleting a contact.
-        // Hashmap <Name, contactObj or PhoneNumber>
-
-        // Create contact objects for testing
-        Contact Amanda = new Contact("Amanda", "5555555");
-        Contact Levi = new Contact("Levi", "2222222");
-        Contact David = new Contact("David", "8675309");
-
-        // Create our list HashMap with our contact objects
-        ContactList list = new ContactList(Amanda, Levi, David);
-
-        list.printContactMap();
-        System.out.println("list.exportMapToList() = " + list.exportMapToList());
 
         String directory = "data";
         String filename = "contacts.txt";
 
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
+        Path filePath = FileIO.createDirectoryAndFile(directory, filename);
+        System.out.println("filePath = " + filePath);
+        System.out.println();
 
-        if (Files.notExists(dataDirectory)) {
-            Files.createDirectories(dataDirectory);
+        ContactList list = new ContactList();
+        List<Contact> importedList = FileIO.importContactFile(filePath);
+
+        for(Contact contact: importedList) {
+            list.addContact(contact);
         }
 
-        if (! Files.exists(dataFile)) {
-            Files.createFile(dataFile);
-        }
+        list.printContactMap();
 
-        //printing to contacts.txt
-//        List<String> contactList = Arrays.asList("name1", "name2", "bacon");
-        Files.write(dataFile, list.exportMapToList());
-
-        //printing to console
-        List<String> fileContents = Files.readAllLines(dataFile);
-        for (int i = 0; i < fileContents.size(); i++) {
-            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-        }
-
+        // TODO make a main loop that is able to control the UX
         printMenu();
         int userChoice = input.getInt("Enter an option (1, 2, 3, 4 or 5):");
         System.out.println("userChoice = " + userChoice);
@@ -64,7 +38,6 @@ public class ContactApp {
         System.out.println("3. Search a contact by name.");
         System.out.println("4. Delete an existing contact.");
         System.out.println("5. Exit.");
-//        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
     }
 }
 
--- src/ContactList.java
+++ src/ContactList.java
@@ -5,10 +5,8 @@ import java.util.List;
 
 public class ContactList {
 
-    // After further testing, wasn't sure we needed an actual arrayList
-    // Decided to simplify and just use a HashMap with names as the key, and contact objects as values
+    // Create a HashMap with names as the key, and contact objects as values
     private HashMap<String, Contact> contactList = new HashMap<>();
-//    private List<String> exportList = Arrays.asList();
 
     public ContactList(Contact...contacts){
         for (Contact contact: contacts){
@@ -39,10 +37,10 @@ public class ContactList {
         System.out.println();
     }
 
+    // The Files.Write method takes a List<String> in order to populate a file, so we need a list of Contact Strings
     public List<String> exportMapToList(){
         List<String> exportList = new ArrayList<>();
         for (String key : contactList.keySet()){
-//            System.out.printf("%-15s | %-15s%n", key, contactList.get(key).getPhoneNumber());
             String contactString = key + " | " + contactList.get(key).getPhoneNumber();
             exportList.add(contactString);
         }
